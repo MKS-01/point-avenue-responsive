75acdd102efc313132afc04b1aa2d1effc8d9d1b
MKS-01 <manish10mks@gmail.com>
2020-08-28 01:09:30 +0530

    final css fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from './components/button';
 import './App.css';
+import Passage from './components/passage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='body'>
+        <div className='header'>
+          <Button text={'Create'} />
+          <Button text='Remove' />
+        </div>
+        <Passage />
+      </div>
     </div>
   );
 }
---
+++ src/components/passage/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+function Passage() {
+  return (
+    <div className='passageView'>
+      <span className='row1'>
+        <br />
+        <br />
+        <br />
+        <br />5
+        <br />
+        <br />
+        <br />
+        <br />
+        <br /> 10
+        <br />
+        <br />
+        <br />
+        <br />
+        <br /> 15
+      </span>
+      <span className='row2'>
+        The Black Bear (Ursus americanus), <br />
+        however clumsy in appearance, is active,
+        <br />
+        vigilant, and persevering; possesses
+        <br />
+        Line great strength, courage, and address; and
+        <br />
+        undergoes with little injury the greatest
+        <br />
+        fatigues and hardships in avoiding the
+        <br />
+        pursuit of the hunter. Like the Deer, it
+        <br />
+        changes its haunts with the seasons, and
+        <br />
+        for the same reason, namely, the desire of
+        <br />
+        obtaining suitable food, or of retiring to
+        <br />
+        the more inaccessible parts, where it can
+        <br />
+        pass the time in security, unobserved by
+        <br />
+        man, the most dangerous of its enemies.
+        <br />
+        During the spring months, it searches for
+        <br />
+        food in the low rich alluvial lands that
+        <br />
+      </span>
+    </div>
+  );
+}
+
+export default Passage;
